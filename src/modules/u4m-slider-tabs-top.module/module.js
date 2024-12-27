bf477a1f3d9c5c8c37c674304e03a9864228e7dd
rakeshm-twp <rakeshm@thewebplant.in>
2024-12-27 19:37:00 +0530

    Header Footere Updates

--- src/modules/u4m-slider-tabs-top.module/module.js
+++ src/modules/u4m-slider-tabs-top.module/module.js
@@ -1,69 +1,71 @@
 $(document).ready(function () {
-  // Tab switching functionality
-  $(".tabber_top_tabs ul li").click(function () {
-    $(this).toggleClass("active").siblings().removeClass("active");
-    var tabId = $(this).children("a").attr("data-tab");
+	// Tab switching functionality
+	$(".tabber_top_tabs ul li").click(function (event) {
+		$(this).toggleClass("active").siblings().removeClass("active");
+		var tabId = $(this).children("a").attr("data-tab");
+		event.preventDefault();
+		// Stop all videos and remove video-enable class when changing tabs
+		$(".tabber_tab_content .tabber_cntnt").each(function () {
+			var dataSrc = $(this).attr("content-id");
+			if (tabId === dataSrc) {
+				$(this).addClass("active").siblings().removeClass("active");
+			} else {
+				// Stop playing video in video tags
+				$(this)
+					.find("video")
+					.each(function () {
+					this.pause();
+					this.currentTime = 0; // Reset to the beginning
+				});
 
-    // Stop all videos and remove video-enable class when changing tabs
-    $(".tabber_tab_content .tabber_cntnt").each(function () {
-      var dataSrc = $(this).attr("content-id");
-      if (tabId === dataSrc) {
-        $(this).addClass("active").siblings().removeClass("active");
-      } else {
-        // Stop playing video in video tags
-        $(this)
-          .find("video")
-          .each(function () {
-            this.pause();
-            this.currentTime = 0; // Reset to the beginning
-          });
+				// Stop iframe videos by clearing their src
+				$(this)
+					.find("iframe")
+					.each(function () {
+					$(this).attr("src", ""); // Clear iframe src
+				});
 
-        // Stop iframe videos by clearing their src
-        $(this)
-          .find("iframe")
-          .each(function () {
-            $(this).attr("src", ""); // Clear iframe src
-          });
+				// Remove the video-enable class
+				$(this).find(".vid_section").removeClass("video-enable");
+			}
+		});
+	});
 
-        // Remove the video-enable class
-        $(this).find(".vid_section").removeClass("video-enable");
-      }
-    });
-  });
+	// Autoplay functionality for videos
+	$(document).on("click", ".poster_img", function () {
+		var parentSection = $(this).closest(".vid_section");
 
-  // Autoplay functionality for videos
-  $(document).on("click", ".poster_img", function () {
-    var parentSection = $(this).closest(".vid_section");
+		if (parentSection.length) {
+			// Add video-enable class
+			parentSection.addClass("video-enable");
 
-    if (parentSection.length) {
-      // Add video-enable class
-      parentSection.addClass("video-enable");
+			// Play HubSpot video
+			var hubspotVideo = parentSection.find(".hubspot_video video").get(0);
+			if (hubspotVideo) {
+				hubspotVideo.play();
+			}
 
-      // Play HubSpot video
-      var hubspotVideo = parentSection.find(".hubspot_video video").get(0);
-      if (hubspotVideo) {
-        hubspotVideo.play();
-      }
-
-      // Autoplay external iframe video
-      var externalIframe = parentSection.find(".external_video iframe");
-      if (externalIframe.length) {
-        var src = externalIframe.attr("data-src");
-        if (src) {
-          externalIframe.attr("src", src);
-        }
-      }
-    }
-  });
+			// Autoplay external iframe video
+			var externalIframe = parentSection.find(".external_video iframe");
+			if (externalIframe.length) {
+				var src = externalIframe.attr("data-src");
+				if (src) {
+					externalIframe.attr("src", src);
+				}
+			}
+		}
+	});
 });
 
 $(".toggle_btn").click(function () {
-  $(this)
-    .next()
-    .slideToggle(250)
-    .parent()
-    .siblings()
-    .children(".tabber_cntnt_row")
-    .slideUp(250);
-  $(this).parent().addClass("active").siblings().removeClass("active");
+	$(this)
+		.next()
+		.slideToggle(250)
+		.parent()
+		.siblings()
+		.children(".tabber_cntnt_row")
+		.slideUp(250);
+	$(this).parent().addClass("active").siblings().removeClass("active");
 });
+
+
