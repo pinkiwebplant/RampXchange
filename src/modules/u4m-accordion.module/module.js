7d331956fba00e1c29295d5b43a153af1a0b2274
amanHubDev <145353514+amanHubDev@users.noreply.github.com>
2025-01-02 17:01:51 +0530

    Accordion update

--- src/modules/u4m-accordion.module/module.js
+++ src/modules/u4m-accordion.module/module.js
@@ -1,10 +1,20 @@
-$('.u4m-accordion .accTitle').click(function() {
-  $(this).parent().siblings('.accItem').removeClass('activeAcc');
-  $(this).parent().siblings('.accItem').find('.accCont').slideUp(250);
-  $(this).next('.accCont').slideToggle(250);
-  $(this).parent().toggleClass('activeAcc');
+// $('.u4m-accordion .accTitle').click(function() {
+//   $(this).parent().siblings('.accItem').removeClass('activeAcc');
+//   $(this).parent().siblings('.accItem').find('.accCont').slideUp(250);
+//   $(this).next('.accCont').slideToggle(250);
+//   $(this).parent().toggleClass('activeAcc');
+//   return false;
+// });
+
+
+$('.u4m-accordion .accItem').click(function() {
+  $(this).siblings('.accItem').removeClass('activeAcc');
+  $(this).siblings('.accItem').find('.accCont').slideUp(250);
+  $(this).children('.accCont').slideToggle(250);
+  $(this).toggleClass('activeAcc');
   return false;
 });
+
 $('.u4m-accordion .accItem').first().addClass('activeAcc');
 $('.u4m-accordion .accItem').first().children('.accCont').slideDown(250);
 
