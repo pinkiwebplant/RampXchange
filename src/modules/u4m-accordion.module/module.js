fe36effb103d2cec09ef0701a56b118f087f10b1
pinkiwebplant <pinki@thewebplant.in>
2025-01-06 12:34:29 +0530

    BUGHURD ISSUE ROUND 2 DONE

--- src/modules/u4m-accordion.module/module.js
+++ src/modules/u4m-accordion.module/module.js
@@ -1,10 +1,4 @@
-// $('.u4m-accordion .accTitle').click(function() {
-//   $(this).parent().siblings('.accItem').removeClass('activeAcc');
-//   $(this).parent().siblings('.accItem').find('.accCont').slideUp(250);
-//   $(this).next('.accCont').slideToggle(250);
-//   $(this).parent().toggleClass('activeAcc');
-//   return false;
-// });
+
 
 
 $('.u4m-accordion .accItem').click(function() {
